a1c6835f9f45d8787025aa06f262a97353667cca
Habinezajanvier <habinezajanvier688@gmail.com>
2020-03-21 14:16:24 +0200

    losting API

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -12,10 +12,12 @@ class Controller {
             owner:{
                 fullName: req.body.ownerName,
                 phoneNumber: req.body.ownerPhoneNumber,
+                email: req.body.ownerEmail
             },
             whoFound:{
                 fullName: req.body.foundName,
-                phoneNumber: req.body.foundPhoneNumber
+                phoneNumber: req.body.foundPhoneNumber,
+                email: req.body.foundEmail
             },
             status: {
                 isFound: true,
@@ -56,8 +58,9 @@ class Controller {
         }
         
     };
+
     static async lostController (req, res){
-        const lost = new Lost({
+        const lost = new LostDocuments({
             documentType: req.body.documentType,
             documentNumber: req.body.documentNumber,
             owner:{
@@ -65,15 +68,54 @@ class Controller {
                 phoneNumber: req.body.ownerPhoneNumber,
                 email: req.body.ownerEmail
             },
-            isRewarded: req.body.reward
+            whoFound:{
+                fullName: req.body.foundName,
+                phoneNumber: req.body.foundPhoneNumber,
+                email: req.body.foundEmail
+            },
+            status: {
+                isLost: true,
+            },
+            location: req.body.location,
+            requireReward: req.body.reward
         });
 
-        try {
-            const lostDoc = await lost.save();
-            res.json(lostDoc);
-        } catch (error) {
-            res.json(error.message);
+        //checking if the lost documents has found before
+        const theFound = await LostDocuments.findOne({
+            $and: [
+                {documentNumber: req.body.documentNumber},
+                {documentType: req.body.documentType},
+                {'status.isFound':true}
+            ]
+        });
+
+        //if the document is found before or to save a lost document
+        if (theFound) {
+            try {
+                const update = await LostDocuments.updateOne({_id: theFound._id}, 
+                    {$set: {'status.isLost':true}});
+    
+                res.json({
+                    msg: `Your ${theFound.documentType} has found by:`,
+                    whoFoundIt: {
+                        Name: theFound.whoFound.fullName,
+                        phoneNumber: theFound.whoFound.phoneNumber,
+                        email: theFound.whoFound.email
+                    }
+    
+                }); 
+            } catch (error) {
+                res.json(error.message);
+            } 
         }
+        else{
+            try {
+                const lostDoc = await lost.save();
+                res.json(lostDoc);
+            } catch (error) {
+                res.json(error.message);
+            }
+        };
     };
 };
 
