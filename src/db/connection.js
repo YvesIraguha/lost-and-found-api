aaad688fbd44a074fa2266e9939f00aaef736443
Habinezajanvier <habinezajanvier688@gmail.com>
2020-03-29 14:48:34 +0200

    refracturing codes

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,8 +5,8 @@ dotenv.config();
 export const db_connections =()=>{
     const db_user = process.env.USER;
     const db_password = process.env.PWD;
-    const db_host = process.env.HOSTNAME;
-    const db_port = process.env.PORTNAME;
+    const db_host = process.env.HOST_NAME;
+    const db_port = process.env.PORT_NAME;
     const db_name = process.env.DB_NAME;
 
     const url = `mongodb://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}?authSource=admin`;
