6452bc533e41690e7af00adb05f15940dbc5e9cd
Habinezajanvier <habinezajanvier688@gmail.com>
2020-04-06 13:19:23 +0200

    chore (configurations)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import  express from 'express';
 
 import {dbConnection} from './db/connection';
 import config from '../config/config';
-import router from './controllers/controller';
 
 require('dotenv').config();
 
@@ -12,7 +11,6 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan('dev'));
-app.use('/lostAndFound', router)
 
 const port = config.app.port;
 
