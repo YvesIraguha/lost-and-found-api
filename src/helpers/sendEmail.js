25d177606a4db7a3308fedb10244fa50d2965e8b
Habinezajanvier <habinezajanvier688@gmail.com>
2020-07-08 20:22:31 +0200

    syncronizaing sending email and updating db

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -1,25 +1,20 @@
-import sgMail from '@sendgrid/mail';
 import dotenv from 'dotenv';
 
 dotenv.config();
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
- const emailNotification = async(doc) =>{
-    const msg = {
-        to:`${doc.user.email}`,
-        from: process.env.SENDGRID_DOMAIN_EMAIL,
-        subject: 'Notification of your lost document',
-        html:`<em> Dear ${doc.user.username},
-              <br> Hope this email finds you well.<br>
-              This is to let you know that your document was found.<br>
-              Stay awesome!</em>`   
-    };
- try{
-     await sgMail.send(msg)
- }catch(error){
-     throw new Error('Email not sent')
- }
-}
-
-export default emailNotification;
+export default (
+  user, email, name, phone
+) => {
+  const msg = {
+    to: user,
+    from: process.env.SENDGRID_DOMAIN_NAME,
+    subject: 'Lost and Found Notification',
+    text: 'Notification for your lost document',
+    html: `<em> Dear ${user},
+                      <br> Hope this email finds you well.<br>
+                      This is to let you know that your document was found by ${name} whose email is ${email} and phone is ${phone}.<br>
+                      Stay awesome!</em>`
+  };
+  return msg;
+};
