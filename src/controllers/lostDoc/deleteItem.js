eab2d51507a47c7203d5b2b28f4beaf6c20bb1ba
Simon97i <softimag08@gmail.com>
2020-07-27 20:47:37 +0200

    handle errors

--- src/controllers/lostDoc/deleteItem.js
+++ src/controllers/lostDoc/deleteItem.js
@@ -4,8 +4,10 @@ export default async (req, res) =>{
     try{
         const deleteDoc = await DeleteItem.findByIdAndDelete(req.params._id)
 
-        return res.status(200).send({msg:`Document with number ${deleteDoc.documentNumber} deleted successfully`, doc});
+        if(!deleteDoc) return res.status(400).send({error: 'No document with such Id found'});
+   
+        return res.status(200).send({msg:`Document with number ${deleteDoc.documentNumber} deleted successfully`, deleteDoc});
     }catch(error){
-        return res.status(400).send({error: 'No document with such Id found'});
+        return res.status(500).send({error:error.message});
     }
 }
