b8f843a5e61b6bd78b6d7cea703b67b133bc819a
Simon97i <softimag08@gmail.com>
2020-07-28 18:50:27 +0200

    enable read lost/found items from DB

--- src/controllers/lostDoc/index.js
+++ src/controllers/lostDoc/index.js
@@ -1,5 +1,9 @@
-import lostItem from './lostItem';
 import foundItem from './foundItem';
+import lostItem from './lostItem';
 import deleteItem from './deleteItem';
+import { allLost, allFound } from '../lostDoc/readItem';
 
-export default { lostItem, foundItem, deleteItem};
+
+export default { lostItem, foundItem, deleteItem, allLost, allFound };
+
+
